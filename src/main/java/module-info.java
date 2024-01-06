fee0b3342d3288bc9731b3b203243eb6e2a42288
Mateusz Zięcina <ziecinamat+github@gmail.com>
2024-01-06 19:47:49 +0100

    Login tab connected with admin tab, added Excel template generation.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,8 @@ module project.materialmanagementsystemjavafx {
     requires com.dlsc.formsfx;
     requires java.sql;
     requires com.opencsv;
+    requires poi;
+    requires poi.ooxml;
 
     opens project.materialmanagementsystemjavafx to javafx.fxml;
     exports project.materialmanagementsystemjavafx;

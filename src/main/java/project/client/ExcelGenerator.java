36a48d93c2de617f5355ad2ed33c46980eebdd5c
Kamil <k.planeta20@gmail.com>
2024-01-13 13:42:07 +0100

    ignore this

--- src/main/java/project/client/ExcelGenerator.java
+++ src/main/java/project/client/ExcelGenerator.java
@@ -6,7 +6,6 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
